2acf7c0951d59ab764499f5baeeb13b636761727
DominikJuraszek <djurasze@gmail.com>
2020-03-21 18:11:21 +0100

    Init commit

---
+++ src/main/java/pl/juraszek/elasticsearchexamples/ElasticsearchExamplesApplication.java
@@ -0,0 +1,13 @@
+package pl.juraszek.elasticsearchexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticsearchExamplesApplication.class, args);
+	}
+
+}
